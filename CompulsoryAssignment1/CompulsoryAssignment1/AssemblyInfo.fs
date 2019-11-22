503e6808b54b566253694c06b1451a0162c31286
ingvarorn87 <ingvarorn87@gmail.com>
2019-11-22 11:10:45 +0100

    Added AssemblyInfo

---
+++ CompulsoryAssignment1/CompulsoryAssignment1/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿namespace CompulsoryAssignment1
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("CompulsoryAssignment1")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+
+()
