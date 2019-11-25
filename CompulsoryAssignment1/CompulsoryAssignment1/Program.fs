1acf7e9f04ac4d580beb386fe680bd125489ec14
ingvarorn87 <ingvarorn87@gmail.com>
2019-11-25 10:38:35 +0100

    Fixed minor errors with insert & minus
    Small spelling fixes and incorrect syntax in minus

--- CompulsoryAssignment1/CompulsoryAssignment1/Program.fs
+++ CompulsoryAssignment1/CompulsoryAssignment1/Program.fs
@@ -30,15 +30,15 @@ let rec insert weaklyACList listItem =
     | x :: xs when listItem > x -> x :: (insert xs listItem)
     | _ -> failwith "Incomplete match on %A"
 //F# Interactive window:
-//  val insert : weaklyACList:'a list -> listItem:'a -> int when 'a : comparison
-//  count[1;1;1;1;2;3;3] 1;;
-//  Expected outcome is , val it : int list = [1;1;1;1;1;2;3;3]
+//  val insert : weaklyACList:'a list -> listItem:'a -> 'a list when 'a : comparison
+//  insert[1;1;1;1;2;3;3] 1;;
+//  Expected outcome is , val it : int list = [1; 1; 1; 1; 1; 2; 3; 3]
 
 
 // Part 3: Intersect
 let intersect (firstList, secondList) = List.filter (fun elm -> List.contains elm firstList) secondList
 //F# Interactive window:
-// val intersect : firstList:'a list * secondList:'a list when 'a : equality
+// val intersect : firstList:'a list * secondList:'a list -> 'a list when 'a : equality
 //intersect([1;2;3;4;4;5],[2;3;4;4]);;
 //val it : int list = [2; 3; 4; 4]
 
@@ -74,5 +74,5 @@ let minus (firstList, secondList) =
 
     //F# Interactive window:
     //val minus : firstList:'a list * secondList:'a list -> 'a list when 'a : equality
-    //minus([1;1;1;2;2:3;3], [1;1;1;2;3]);;
+    //minus([1;1;1;2;3] , [1;1;1;2;2;3;3]);;
     //val it : int list = [2; 3]
