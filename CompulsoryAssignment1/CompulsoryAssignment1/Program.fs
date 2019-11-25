2519d96149fbc2374a730ad847b0845cb2079aee
ingvarorn87 <ingvarorn87@gmail.com>
2019-11-25 09:39:00 +0100

    Part 1 and 2
    Count and insert

--- CompulsoryAssignment1/CompulsoryAssignment1/Program.fs
+++ CompulsoryAssignment1/CompulsoryAssignment1/Program.fs
@@ -1,8 +1,30 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Linq
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+
+// Part 1: count
+let rec count weaklyACList listItem =
+    match weaklyACList with
+    | [] -> 0
+    | x :: [] ->
+        if x = listItem then 1
+        else 0
+    | x :: xs ->
+        (if x = listItem then 1
+         else 0)
+        + count xs listItem
+
+
+// Part 2: Insert
+let rec insert weaklyACList listItem =
+    match weaklyACList with
+    | [] -> [ listItem ]
+    | x :: [] when listItem <= x -> [ listItem ] @ [ x ]
+    | x :: [] when listItem > x -> [ x ] @ [ listItem ]
+    | x :: xs when listItem <= x -> listItem :: [ x ] @ xs
+    | x :: xs when listItem > x -> x :: (insert xs listItem)
+    | _ -> failwith "Incomplete match on %A"
+
+
